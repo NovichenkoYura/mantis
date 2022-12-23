55de89c2e855b1b2009cdcbc92461607a6abb058
NovichenkoYura <novichenko.y@gmail.com>
2022-12-23 09:41:48 +0200

    1

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider
+} from 'react-query';
 
-function App() {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    },
+    mutations: {}
+  }
+});
+
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <Todos />
+      </QueryClientProvider>
     </div>
   );
-}
-
-export default App;
+};
