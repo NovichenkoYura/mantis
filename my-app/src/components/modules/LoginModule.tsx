ef44959b10dc0b1a6a78ee048840fdf91253140c
NovichenkoYura <novichenko.y@gmail.com>
2022-12-27 14:17:18 +0200

    1

--- my-app/src/components/modules/LoginModule.tsx
+++ my-app/src/components/modules/LoginModule.tsx
@@ -1,49 +1,80 @@
-import { Button, Dialog, DialogActions, DialogContent, TextField, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardMedia,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  TextField,
+  Typography
+} from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
+import { Box } from '@mui/system';
 
 export const LoginModule = () => {
   return (
-    <Dialog open aria-label="login">
-      <DialogContent>
-        <img src="https://my.mantis.com.ua/assets/images/logo/shop_logo_big.png" alt="" />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Login"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="password"
-          label="Password"
-          type="password"
-          fullWidth
-          variant="standard"
-        />
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox
-                defaultChecked
-                sx={{
-                  '& .MuiSvgIcon-root': { fontSize: 28 }
-                }}
-              />
-            }
-            label="Запам'ятати"
+    <Card sx={{ height: '100%' }}>
+      <CardMedia
+        component="img"
+        src="https://my.mantis.com.ua/assets/images/backgrounds/i2.jpg"
+        sx={{ height: '100%' }}
+      />
+      <Dialog open aria-label="login">
+        <DialogContent>
+          <img
+            src="https://my.mantis.com.ua/assets/images/logo/shop_logo_big.png"
+            alt=""
+            className="login__form__img"
           />
-        </FormGroup>
-        <Typography variant="subtitle1">Забули пароль?</Typography>
-        <DialogActions>
-          <Button variant="contained">Увійти</Button>
-        </DialogActions>
-      </DialogContent>
-    </Dialog>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Login"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  defaultChecked
+                  sx={{
+                    '& .MuiSvgIcon-root': { fontSize: 28 }
+                  }}
+                />
+              }
+              label="Запам'ятати"
+            />
+          </FormGroup>
+          <Typography variant="subtitle1">Забули пароль?</Typography>
+          <DialogActions>
+            <Box>
+              <Button
+                variant="contained"
+                sx={{
+                  width: 214,
+                  height: 36
+                }}>
+                Увійти
+              </Button>
+            </Box>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </Card>
   );
 };
