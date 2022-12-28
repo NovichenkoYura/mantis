f9406b005810d821ea8d409ffd34710e26b1fd99
NovichenkoYura <novichenko.y@gmail.com>
2022-12-28 21:07:15 +0200

    1

--- my-app/src/components/modules/LoginModule.tsx
+++ my-app/src/components/modules/LoginModule.tsx
@@ -14,6 +14,32 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
 import { Box } from '@mui/system';
 
+import { createTheme } from '@mui/material/styles';
+
+import { purple } from '@mui/material/colors';
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: purple[500]
+//     },
+//     secondary: {
+//       main: '#ff9800'
+//     }
+//   }
+// });
+
+const theme = createTheme({
+  palette: {
+    success: {
+      main: purple[500]
+    },
+    secondary: {
+      main: '#ff9800'
+    }
+  }
+});
+
 export const LoginModule = () => {
   return (
     <Card sx={{ height: '100%' }}>
@@ -47,27 +73,39 @@ export const LoginModule = () => {
             fullWidth
             variant="standard"
           />
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  defaultChecked
-                  sx={{
-                    '& .MuiSvgIcon-root': { fontSize: 28 }
-                  }}
-                />
-              }
-              label="Запам'ятати"
-            />
-          </FormGroup>
-          <Typography variant="subtitle1">Забули пароль?</Typography>
+          <Box width="100%" display="flex" justifyContent="space-between" alignItems="center">
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    defaultChecked
+                    sx={{
+                      '& .MuiSvgIcon-root': { fontSize: 28 },
+                      '&.Mui-checked': {
+                        color: 'rgb(251,140,0)'
+                      }
+                    }}
+                  />
+                }
+                label="Запам'ятати"
+              />
+            </FormGroup>
+            <Typography variant="subtitle1">Забули пароль?</Typography>
+          </Box>
+
           <DialogActions>
-            <Box>
+            <Box width="100%" display="flex" justifyContent="center" alignItems="center">
               <Button
                 variant="contained"
+                color="primary"
                 sx={{
                   width: 214,
-                  height: 36
+                  height: 36,
+                  color: 'rgba(0,0,0,0.87)',
+                  backgroundColor: 'rgb(251,140,0)',
+                  '&:hover': {
+                    backgroundColor: '#b26a00'
+                  }
                 }}>
                 Увійти
               </Button>
