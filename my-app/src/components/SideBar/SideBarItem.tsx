e44741daf14bdae7b95631ccb86f902a3fe1cd37
NovichenkoYura <novichenko.y@gmail.com>
2023-01-06 14:38:01 +0200

    1

--- my-app/src/components/SideBar/SideBarItem.tsx
+++ my-app/src/components/SideBar/SideBarItem.tsx
@@ -14,94 +14,96 @@ import ArticleIcon from '@mui/icons-material/Article';
 import UnarchiveIcon from '@mui/icons-material/Unarchive';
 import DataObjectRoundedIcon from '@mui/icons-material/DataObjectRounded';
 import ReadMoreSharpIcon from '@mui/icons-material/ReadMoreSharp';
+import { StyledListItem } from './listItemStyles';
 
 export const SideBarItem = () => {
   return (
     <>
-      <ListItem>
+      <StyledListItem>
         <ListItemAvatar>
-          <Avatar sx={{ bgcolor: 'transparent' }}>
+          <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
             <WindowSharpIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="Main"
-          sx={{ color: 'white' }}
+          sx={{ color: '#e0e0e0' }}
           primaryTypographyProps={{ fontSize: '13px' }}
         />
-      </ListItem>
-      <ListItem>
+      </StyledListItem>
+
+      <StyledListItem>
         <ListItemAvatar>
-          <Avatar sx={{ bgcolor: 'transparent' }}>
+          <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
             <NewspaperSharpIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="News"
-          sx={{ color: 'white' }}
+          sx={{ color: '#e0e0e0' }}
           primaryTypographyProps={{ fontSize: '13px' }}
         />
-      </ListItem>
-      <ListItem>
+      </StyledListItem>
+      <StyledListItem>
         <ListItemAvatar>
-          <Avatar sx={{ bgcolor: 'transparent' }}>
+          <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
             <InfoOutlinedIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="Information"
-          sx={{ color: 'white' }}
+          sx={{ color: '#e0e0e0' }}
           primaryTypographyProps={{ fontSize: '13px' }}
         />
-      </ListItem>
-      <ListItem>
+      </StyledListItem>
+      <StyledListItem>
         <ListItemAvatar>
-          <Avatar sx={{ bgcolor: 'transparent' }}>
+          <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
             <ViewInArSharpIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="Goods"
-          sx={{ color: 'white' }}
+          sx={{ color: '#e0e0e0' }}
           primaryTypographyProps={{ fontSize: '13px' }}
         />
-      </ListItem>
-      <ListItem>
+      </StyledListItem>
+      <StyledListItem>
         <ListItemAvatar>
-          <Avatar sx={{ bgcolor: 'transparent' }}>
+          <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
             <ArticleIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
-          primary="Documments"
-          sx={{ color: 'white' }}
+          primary="Documents"
+          sx={{ color: '#e0e0e0' }}
           primaryTypographyProps={{ fontSize: '13px' }}
         />
-      </ListItem>
-      <ListItem>
+      </StyledListItem>
+      <StyledListItem>
         <ListItemAvatar>
-          <Avatar sx={{ bgcolor: 'transparent' }}>
+          <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
             <UnarchiveIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="Preorders"
-          sx={{ color: 'white' }}
+          sx={{ color: '#e0e0e0' }}
           primaryTypographyProps={{ fontSize: '13px' }}
         />
-      </ListItem>
-      <ListItem>
+      </StyledListItem>
+      <StyledListItem>
         <ListItemAvatar>
-          <Avatar sx={{ bgcolor: 'transparent' }}>
+          <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
             <DataObjectRoundedIcon />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="API"
-          sx={{ color: 'white' }}
+          sx={{ color: '#e0e0e0' }}
           primaryTypographyProps={{ fontSize: '13px' }}
         />
-      </ListItem>
+      </StyledListItem>
     </>
   );
 };
