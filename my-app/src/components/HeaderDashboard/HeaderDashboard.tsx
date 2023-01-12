508c3f7b6fa3751e9fadca7e56ca2297968aea7d
NovichenkoYura <novichenko.y@gmail.com>
2023-01-12 17:11:23 +0200

    1

--- my-app/src/components/HeaderDashboard/HeaderDashboard.tsx
+++ my-app/src/components/HeaderDashboard/HeaderDashboard.tsx
@@ -22,51 +22,58 @@ export const HeaderDashboard = () => {
   });
 
   return (
-    <AppBar color="transparent" sx={{ boxShadow: 1 }}>
-      <StyledToolbar>
-        <Divider orientation="vertical" flexItem />
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="transparent" sx={{ boxShadow: 1 }}>
+        <StyledToolbar>
+          <Divider orientation="vertical" flexItem />
 
-        <StyledListItem sx={{ width: 1, height: 1 }}>
-          <ListItemAvatar>
-            <Avatar sx={{ bgcolor: 'transparent', color: '#e0e0e0' }}>
-              <PhoneSharpIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary="066-655-22-33"
-            sx={{ color: '#e0e0e0' }}
-            primaryTypographyProps={{ fontSize: '13px' }}
-          />
-        </StyledListItem>
-        <Divider orientation="vertical" flexItem />
-        <Box
-          sx={{
-            width: '280px',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between'
-          }}>
-          <Typography component="div" sx={{ pl: 3 }}>
-            € 42.51
-          </Typography>
-          <Typography component="div" sx={{ pr: 3 }} id="curencyRate">
-            RRC: 40.00
-          </Typography>
-        </Box>
+          <ListItem
+            sx={{ width: '208px', display: 'flex', flexDirection: 'row', textAlign: 'right' }}>
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: 'transparent', color: '#616161', textAlign: 'right' }}>
+                <PhoneSharpIcon />
+              </Avatar>
+            </ListItemAvatar>
+            {/* <Typography>066-655-22-33</Typography> */}
+            <ListItemText
+              primary="066-655-22-33"
+              sx={{ color: 'rgb(251, 140, 0)', bgcolor: 'transparent' }}
+              primaryTypographyProps={{ fontSize: '17px' }}
+            />
+          </ListItem>
+          <Divider orientation="vertical" flexItem />
+          <Box
+            sx={{
+              width: '208px',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }}>
+            <Typography component="div" sx={{ pl: 3 }}>
+              € 42.51
+            </Typography>
+            <Typography component="div" sx={{ pr: 3 }} id="curencyRate">
+              RRC: 40.00
+            </Typography>
+          </Box>
 
-        <Divider orientation="vertical" flexItem />
+          <Divider orientation="vertical" flexItem />
 
-        <IconButton sx={{ width: 52, height: 64, p: 0 }}>
-          <AttachFileSharpIcon />
-        </IconButton>
-        <Divider orientation="vertical" flexItem />
-        <IconButton sx={{ width: 52, height: 64, p: 0 }}>
-          <PersonIcon />
-        </IconButton>
-        <IconButton sx={{ width: 76, height: 64, p: 0 }}>
-          <ShoppingCartIcon />
-        </IconButton>
-      </StyledToolbar>
-    </AppBar>
+          <IconButton
+            sx={{ width: 52, height: 64, p: 0, bgcolor: 'transparent', color: '#616161' }}>
+            <AttachFileSharpIcon />
+          </IconButton>
+          <Divider orientation="vertical" flexItem />
+          <IconButton
+            sx={{ width: 52, height: 64, p: 0, bgcolor: 'transparent', color: '#616161' }}>
+            <PersonIcon />
+          </IconButton>
+          <IconButton
+            sx={{ width: 76, height: 64, p: 0, bgcolor: 'transparent', color: '#616161' }}>
+            <ShoppingCartIcon />
+          </IconButton>
+        </StyledToolbar>
+      </AppBar>
+    </Box>
   );
 };
