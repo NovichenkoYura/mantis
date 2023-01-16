670b2c8de62974d6f98eeb0b4eb06a1efa90c598
NovichenkoYura <novichenko.y@gmail.com>
2023-01-16 07:47:24 +0200

    1

---
+++ my-app/src/components/HeaderStartPage/HeaderStartPage.tsx
@@ -0,0 +1,42 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { locations } from 'constans/locations';
+import { NavLink } from 'react-router-dom';
+export const HeaderStartPage = () => {
+  return (
+    <>
+      <AppBar position="fixed" sx={{ bgcolor: '#424242', opacity: 0.6 }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            color="#ffb400"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+            MANTIS UKRAINE
+          </Typography>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+            B2B & DISTRIBUTION
+          </Typography>
+
+          <div className="header__title__link__wrap">
+            <NavLink to={locations.dashboard.path} className="header__title__link">
+              DASHBOARD
+            </NavLink>
+            <NavLink to={locations.login.path} className="header__title__link">
+              ENGLISH
+            </NavLink>
+            <NavLink to={locations.login.path} className="header__title__link">
+              ФОРУМ
+            </NavLink>
+            <NavLink to={locations.login.path} className="header__title__link">
+              ВХІД ДЛЯ ДИЛЕРІВ
+            </NavLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
