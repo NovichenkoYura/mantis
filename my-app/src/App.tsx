60b986663cd91f3519ea94c84bae8884e5fa5326
NovichenkoYura <novichenko.y@gmail.com>
2023-01-18 03:36:27 +0200

    1

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -16,40 +16,10 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useMemo } from 'react';
 import { loginThunk } from 'store/usersSlice';
-interface formProps {
-  login?: string;
-  password?: string;
-}
 
 const queryClient = new QueryClient();
 
-export const App: React.FC<formProps> = () => {
-  const dispatch = useAppDispatch();
-
-  const validationSchema = useMemo(() => {
-    return Yup.object({
-      login: Yup.string().required('Required'),
-      password: Yup.string().min(5).max(500).required('Required')
-    });
-  }, []);
-
-  const formik = useFormik({
-    initialValues: {
-      login: '',
-      password: ''
-    },
-    onSubmit: (values, { resetForm }) => {
-      console.log(values);
-      dispatch(
-        loginThunk({
-          login: values.login,
-          password: values.password
-        })
-      );
-      resetForm();
-    },
-    validationSchema
-  });
+export const App = () => {
   return (
     <>
       <div className="App">
